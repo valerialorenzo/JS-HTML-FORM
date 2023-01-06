aca14043d987cc8478c512bb3a660cfbc70940a1
valerialorenzo <valerialorenzo9@gmail.com>
2023-01-06 18:35:06 +0000

    html form js

--- src/app.js
+++ src/app.js
@@ -2,72 +2,121 @@
 import "bootstrap";
 import "./style.css";
 
-//dot notion
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
 window.onload = function() {
   //write your code here
+  console.log("Hello Rigo from the console!");
+  var form = document.getElementById("form");
 
-  console.log("Hello");
+  form.addEventListener("submit", prueba);
 
-  //agregamos un evento al elemento form, primero obtenemos el elemento
-  let form = document.querySelector("form");
-  //Luego manipulamos el elemento agregandole el evento submit
-  // form.addEventListener("evento",funcion que se va activar)
-  //on
-  form.addEventListener("submit", enviarDatos);
+  // Inputs
+  let amount = document.getElementById("inputAmount");
+  let card = document.getElementById("inputCard");
+  let cvc = document.getElementById("inputCVC");
+  let firstname = document.getElementById("inputFName");
+  let lastname = document.getElementById("inputLName");
+  let city = document.getElementById("inputCity");
+  let state = document.getElementById("inputState");
+  let postalcode = document.getElementById("inputZip");
+  let message = document.querySelector("textarea");
+  // Inputs
 
-  //declaramos la funcion que envia los datos del formulario
-  function enviarDatos(e) {
-    e.preventDefault(); // detiene tu comportamiento predeterminado del formulario e interpretado mi codigo
-    let amount = document.getElementById("inputAmount");
-    let card = document.getElementById("inputCard");
-    let cvc = document.getElementById("inputCVC");
-    let firstname = document.getElementById("inputFName");
-    let lastname = document.getElementById("inputLName");
-    let city = document.getElementById("inputCity");
-    let state = document.getElementById("inputState");
-    let postalcode = document.getElementById("inputZip");
-    let message = document.getElementsByClassName("form-control");
-    let pagos = document.getElementsByClassName("form-check-input");
+  // Error messages
+  let cardErr = document.getElementById("cardErrMsg");
+  let cvcErr = document.getElementById("cvcErrMsg");
+  let amountErr = document.getElementById("amountErrMsg");
+  let fNameErr = document.getElementById("fNameErrMsg");
+  let lNameErr = document.getElementById("lNameErrMsg");
+  let cityErr = document.getElementById("cityErrMsg");
+  let stateErr = document.getElementById("stateErrMsg");
+  let zipErr = document.getElementById("zipErrMsg");
+  let cardSelectErr = document.getElementById("cardselectErrMsg");
+  let messageErr = document.getElementById("messageErrMsg");
+  // End of error messages
 
+  function prueba(e) {
+    e.preventDefault();
     if (amount.value === "") {
-      console.log("El campo amount está vacio");
+      amountErr.classList.remove("invisible");
+      amount.style.background = "pink";
+    } else {
+      amountErr.classList.add("invisible");
+      amount.style.background = "#fff";
     }
 
     if (card.value === "") {
-      console.log("El campo card está vacio");
+      cardErr.classList.remove("invisible");
+      card.style.background = "pink";
+    } else {
+      cardErr.classList.add("invisible");
+      card.style.background = "#fff";
     }
 
     if (cvc.value === "") {
-      console.log("El campo cvc está vacio");
+      cvcErr.classList.remove("invisible");
+      cvc.style.background = "pink";
+    } else {
+      cvcErr.classList.add("invisible");
+      cvc.style.background = "#fff";
     }
+
     if (firstname.value === "") {
-      console.log("El campo firstname está vacio");
+      fNameErr.classList.remove("invisible");
+      firstname.style.background = "pink";
+    } else {
+      fNameErr.classList.add("invisible");
+      firstname.style.background = "#fff";
     }
 
     if (lastname.value === "") {
-      console.log("El campo lastname está vacio");
+      lNameErr.classList.remove("invisible");
+      lastname.style.background = "pink";
+    } else {
+      lNameErr.classList.add("invisible");
+      lastname.style.background = "#fff";
     }
 
     if (city.value === "") {
-      console.log("El campo city está vacio");
-    }
-    if (state.value === "Pick a state") {
-      console.log("El campo state está vacio");
+      cityErr.classList.remove("invisible");
+      city.style.background = "pink";
+    } else {
+      cityErr.classList.add("invisible");
+      city.style.background = "#fff";
     }
 
     if (postalcode.value === "") {
-      console.log("El campo postalcode está vacio");
+      zipErr.classList.remove("invisible");
+      postalcode.style.background = "pink";
+    } else {
+      zipErr.classList.add("invisible");
+      postalcode.style.background = "#fff";
     }
+
     if (message.value === "") {
-      console.log("El campo message está vacio");
+      messageErr.classList.remove("invisible");
+      message.style.background = "pink";
+    } else {
+      messageErr.classList.add("invisible");
+      message.style.background = "#fff";
     }
 
     let elementoActivo = document.querySelector(
       'input[name="inlineRadioOptions"]:checked'
     );
-    if (elementoActivo) {
+    if (state.value === "Pick a state") {
+      stateErr.classList.remove("invisible");
+      state.style.background = "pink";
     } else {
-      console.log("El campo target esta vacio");
+      stateErr.classList.add("invisible");
+      state.style.background = "#fff";
+    }
+    if (!elementoActivo) {
+      cardSelectErr.classList.remove("invisible");
+    } else {
+      cardSelectErr.classList.add("invisible");
     }
   }
 };
